d75e3b449e58ce1cca3ba1a7f537ab6c5832f262
Shantanu Oak <shantanu.oak@gmail.com>
2015-06-06 22:50:18 +0530

    Update code.py
    added access to third server

--- code.py
+++ code.py
@@ -21,7 +21,7 @@ urls = ('/', 'index')
 app = web.application(urls, globals())
 
 myform = form.Form(
-    form.Dropdown('mydrop', [('value1', 'first server'), ('value2', 'second server')]),
+    form.Dropdown('mydrop', [('value1', 'first server'), ('value2', 'second server'), ('value3', 'third server')]),
     form.Textbox("middle_server"),
     form.Textbox("middle_port",
         form.regexp('\d+', 'Usually port 22')),
@@ -32,6 +32,12 @@ myform = form.Form(
     form.Textbox("second_port"),
     form.Textbox('second_user'),
     form.Textbox('second_password'),
+
+    form.Textbox("third_server"),
+    form.Textbox("third_port"),
+    form.Textbox('third_user'),
+    form.Textbox('third_password'),
+
     form.Textbox('exec_command'),
     form.Textbox('file_receive'),
     form.Textbox('file_receive_location'),
@@ -158,6 +164,132 @@ def mysecondconnect(middle_server, middle_port, middle_user, middle_password, se
     return myreturn
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def mythirdconnect(middle_server, middle_port, middle_user, middle_password, second_server, second_port, second_user, second_password, third_server, third_port, third_user, third_password, mycommand, file_send, file_rec, rece_f_loc, send_f_loc):
+    myreturn=''
+    try:
+        import paramiko
+    except:
+        print "import error"
+    try:
+        proxy_client = paramiko.SSHClient()
+        proxy_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    except:
+        print "error1 "
+    myline= "proxy_client.connect('%s', port=%s, username='%s',  password='%s')" % (middle_server, int(middle_port), middle_user, middle_password)
+    try:
+        exec myline
+    except:
+        print "error myfirstconnect in function mysecondconnect"
+        exit
+    transport = proxy_client.get_transport()
+    try:
+        myline2="dest_addr = ('%s', %d)" % (second_server, int(second_port))
+        print myline2
+        exec myline2
+    except:
+        print "error in myline2"
+    try:
+        local_addr = ('127.0.0.1', 1234)
+        channel = transport.open_channel("direct-tcpip", dest_addr, local_addr)
+        last_server = paramiko.SSHClient()
+        last_server.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        myline3="last_server.connect(hostname='localhost', username='%s', password='%s', sock=channel)" % (second_user, second_password)
+        exec myline3
+        print myline3
+    except:
+        print "error in myline3"
+###
+    transport1 = last_server.get_transport()
+    try:
+        myline2="dest_addr1 = ('%s', %d)" % (third_server, int(third_port))
+        print myline2
+        exec myline2
+    except:
+        print "error in myline2"
+    try:
+        local_addr = ('127.0.0.1', 1234)
+        channel1 = transport1.open_channel("direct-tcpip", dest_addr1, local_addr)
+        etim_server = paramiko.SSHClient()
+        etim_server.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        myline3="etim_server.connect(hostname='localhost', username='%s', password='%s', sock=channel1)" % (third_user, third_password)
+        exec myline3
+        print myline3
+    except:
+        print "error in myline3"
+
+    try:
+        sftp = etim_server.open_sftp()
+        sftp.put(file_send, send_f_loc)
+        sftp.close()
+    except:
+        print "error while put %s %s " % (file_send, send_f_loc)
+        put_error = "error while sending file %s to %s " % (file_send, send_f_loc)
+        #tkMessageBox.showerror("command failed", put_error)
+
+    try:
+        (sshin1, sshout1, ssherr1) = etim_server.exec_command(mycommand)
+        mytext=sshout1.read()
+        mytext_error=ssherr1.read()
+        if mytext:
+            myreturn += mytext
+            #tkMessageBox.showinfo("command output", mytext)
+        elif mytext_error:
+            myreturn += mytext_error
+            #tkMessageBox.showerror("command failed", mytext_error)
+    except:
+        print "error while executing the command on remote host"
+
+    try:
+        sftp = etim_server.open_sftp()
+        sftp.get(file_rec, rece_f_loc)
+        sftp.close()
+    except:
+        print "error while get %s %s " % (file_rec, rece_f_loc)
+        get_error = "error while receiving file from %s to %s " % (file_rec, rece_f_loc)
+        #tkMessageBox.showerror("command failed", get_error)
+
+    return myreturn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class index:
     def GET(self):
         form = myform()
@@ -182,6 +314,10 @@ class index:
             second_port=form['second_port'].value.encode('ascii', 'ignore') or None
             second_user=form['second_user'].value.encode('ascii', 'ignore') or None
             second_password=form['second_password'].value.encode('ascii', 'ignore') or None
+            third_server=form['third_server'].value.encode('ascii', 'ignore') or None
+            third_port=form['third_port'].value.encode('ascii', 'ignore') or None
+            third_user=form['third_user'].value.encode('ascii', 'ignore') or None
+            third_password=form['third_password'].value.encode('ascii', 'ignore') or None
             user_option=form['mydrop'].value
             file_rec=form['file_receive'].value.encode('ascii', 'ignore') or None
             rece_f_loc=form['file_receive_location'].value.encode('ascii', 'ignore') or None
@@ -189,7 +325,9 @@ class index:
             send_f_loc=form['file_send_location'].value.encode('ascii', 'ignore') or None
 
             print middle_server.encode('ascii','ignore'), middle_port, middle_user, middle_password, mycommand, file_send, file_rec, rece_f_loc, send_f_loc
-            if user_option == 'value2':
+            if user_option == 'value3':
+                toshow=mythirdconnect(middle_server, middle_port , middle_user , middle_password , second_server, second_port, second_user, second_password, third_server, third_port, third_user, third_password, mycommand , file_send, file_rec, rece_f_loc, send_f_loc)
+            elif user_option == 'value2':
                 toshow=mysecondconnect(middle_server, middle_port , middle_user , middle_password , second_server, second_port, second_user, second_password, mycommand , file_send, file_rec, rece_f_loc, send_f_loc)
             else:
                 print "error myscondconnect"
@@ -200,3 +338,4 @@ class index:
 if __name__=="__main__":
     web.internalerror = web.debugerror
     app.run()
+
